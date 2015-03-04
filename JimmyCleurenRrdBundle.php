db86dcd0f1dd436a3237a6a0769a7d45b14fc720
Jimmy Cleuren <jimmy.cleuren@cegeka.be>
2015-03-04 15:52:05 +0100

    bundle generation

---
+++ JimmyCleurenRrdBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace JimmyCleuren\RrdBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JimmyCleurenRrdBundle extends Bundle
+{
+    /**
+     * {@inheritDoc}
+     * @param ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+
+    }
+}
